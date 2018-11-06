ca5c006ed37f773bec55c8e1d620e4ec094e963a
tony <tonycs_y@126.com>
2018-11-06 16:19:21 +0800

    fix

--- src/app/pages/share.module.ts
+++ src/app/pages/share.module.ts
@@ -3,18 +3,20 @@ import {CommonModule} from '@angular/common';
 import {ReactiveFormsModule} from '@angular/forms';
 import {ModalModule} from '../modal/modal.module';
 import {PageNoFoundComponent} from './common/page-no-found/page-no-found.component';
+import {HighLightDirective} from '../core/directive/high-light.directive';
 
 @NgModule({
     imports: [
         CommonModule,
         ReactiveFormsModule,
         ModalModule,
+        HighLightDirective,
     ],
     declarations: [
         PageNoFoundComponent,
     ],
     exports: [
-    
+        HighLightDirective,
     ]
 })
 export class ShareModule {
