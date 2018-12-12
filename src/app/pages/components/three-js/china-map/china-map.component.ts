d7aeec3673f6d7afccbfbdbb88fc11215f4d734e
tony <tonycs_y@126.com>
2018-12-12 18:33:19 +0800

    fix

--- src/app/pages/components/three-js/china-map/china-map.component.ts
+++ src/app/pages/components/three-js/china-map/china-map.component.ts
@@ -8,7 +8,7 @@ import * as d3Color from 'd3-scale-chromatic';
 const width = 1024;
 const height = 600;
 
-const projection = geo.geoNaturalEarth1()
+const projection = geo.geoMercator()
     .scale(550)
     .center([105, 38])
     .translate([width / 2, height / 2]);
@@ -50,24 +50,24 @@ export class ChinaMapComponent implements OnInit, AfterViewInit {
     }
     
     drawMapSvg() {
-        const svg = d3.select('#svg')
-            .attr('width', width)
-            .attr('height', height);
         // 9个自然颜色
         // const colors = d3.scaleOrdinal(d3Color.schemePaired);
         // 单一颜色过度
         // const colors = d3.scaleOrdinal(d3Color.schemeOranges[9]);
         //
         const colors = d3.scaleOrdinal(d3Color.schemeBrBG[11]);
-        
+        // 路径生成器
         const path = geo.geoPath(projection);
+        
+        const svg = d3.select('#svg')
+            .attr('width', width)
+            .attr('height', height);
         this.getJson().then(data => {
-            console.log(svg.selectAll('path'));
             svg.selectAll('path')
                 .data(data.features)
                 .enter()
                 .append('path').attr('d', path)
-                .attr('fill', function (d, i) {
+                .attr('fill', (d, i) => {
                     return colors(i);
                 })
                 .attr('stroke', 'rgba(255, 255, 255, 1')
@@ -90,10 +90,7 @@ export class ChinaMapComponent implements OnInit, AfterViewInit {
                     'lat': '22'
                 }
             ];
-            
             const tooltip = d3.select('#tooltip');
-            
-            
             
             const location = svg.selectAll('.location')
                 .data(places)
@@ -103,32 +100,29 @@ export class ChinaMapComponent implements OnInit, AfterViewInit {
                 .attr('transform', (d) => {
                     const coor = projection([d.log, d.lat]);
                     return 'translate(' + coor[0] + ',' + coor[1] + ')';
-                })
-                .on('mouseover', function(d) {
-                    console.log(d);
-                    tooltip.html('当前城市：' + d.name)
-                        .style('left', d3.event.pageX + 20 + 'px')
-                        .style('top', d3.event.pageY + 20 + 'px')
-                        .style('opacity', 1);
-                    console.log('this', this);
-                    console.log('select', d3.select(this).select('circle'));
-                    
-                    d3.select(this).select('circle').transition()
-                        .duration(150)
-                        .attr('r', 8);
-                })
-                .on('mouseout', function() {
-                    tooltip.style('opacity', 0);
-                    d3.select(this)
-                        .select('circle')
-                        .transition()
-                        .duration(150)
-                        .attr('r', 4);
                 });
+            
             location.append('circle')
                 .attr('r', 4)
                 .attr('fill', '#e91e63')
                 .attr('class', 'location');
+            
+            location.on('mouseover', function (d) {
+                tooltip.html('当前城市：' + d.name)
+                    .style('left', d3.event.pageX + 20 + 'px')
+                    .style('top', d3.event.pageY + 20 + 'px')
+                    .style('opacity', 1);
+                d3.select(this).select('circle').transition()
+                    .duration(150)
+                    .attr('r', 8);
+            }).on('mouseout', function () {
+                tooltip.style('opacity', 0);
+                d3.select(this)
+                    .select('circle')
+                    .transition()
+                    .duration(150)
+                    .attr('r', 4);
+            });
         });
     }
     
